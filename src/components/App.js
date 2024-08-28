c22ef6cd3d25aed953547261be70df46d08eee42
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-08-28 12:01:49 +0530

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -3,25 +3,31 @@
 import React from 'react'
 
 import '../styles/App.css'
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
 const App = () => {
-    const[text, setText] = useState('')
-    const[loadingText, setLoadingText] = useState('')
+    const [text, setText] = useState('')
+    const [loadingText, setLoadingText] = useState('')
 
     useEffect(() => {
         setLoadingText(text);
-    },[text])
-  return (
-    <div className="app" style={{width:'100vw', height:'100vh', display:'flex', justifyContent:'center', alignItems:'center'}}>
-     <div className='textarea' style={{width:'50%', height:'100%'}}>
-        <textarea value={text} onChange={(e) => setText(e.target.value)} style={{ width:'98%', height:'100%', padding:'10px'}} />
+    }, [text])
+    return (
+        <div className="app" style={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <div className='textarea' style={{ width: '50%', height: '100%' }}>
+                <textarea value={text} onChange={(e) => setText(e.target.value)} style={{ width: '98%', height: '100%', padding: '10px' }} />
 
-        </div>
-     <div className='preview' style={{width:'50%', height:'100%', backgroundColor: 'lightgray'}}>
-        <h1 className='loading' style={{fontWeight:'bold', padding:'10px'}}>{loadingText}</h1>
+            </div>
+            <div className='preview' style={{ width: '50%', height: '100%', backgroundColor: 'lightgray' }}>
+                <h1 className='loading' style={{
+                    padding: '10px',
+                    maxWidth: '100%',
+                    height: 'auto',  
+                    wordWrap: 'break-word',  
+                    overflowWrap: 'break-word',  
+                    display: 'block'  }}>{loadingText}</h1>
      </div>
-    </div>
+    </div >
   )
 }
 
