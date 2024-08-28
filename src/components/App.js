eb96f0eb47987b1eac5c480491c77fed5d4a8a25
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-08-28 12:42:09 +0530

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,24 @@
-
-
-import React from 'react'
-
-import '../styles/App.css'
-import { useState, useEffect } from 'react'
-import ReactMarkdown from 'react-markdown'
+import React, { useState } from 'react';
+import '../styles/App.css';
+import ReactMarkdown from 'react-markdown';
 
 const App = () => {
-    const [text, setText] = useState('')
-    const [loadingText, setLoadingText] = useState('')
-
-    useEffect(() => {
-        setLoadingText(text)
-    }, [text])
+    const [text, setText] = useState('');
 
     return (
         <div className="app" style={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <div className='textarea' style={{ width: '50%', height: '100%' }}>
-                <textarea value={text} onChange={(e) => setText(e.target.value)} style={{ width: '98%', height: '100%', padding: '10px' }} />
+                <textarea 
+                    value={text} 
+                    onChange={(e) => setText(e.target.value)} 
+                    style={{ width: '98%', height: '100%', padding: '10px' }} 
+                />
             </div>
             <div className='preview' style={{ width: '50%', height: '100%', backgroundColor: 'lightgray', padding: '10px' }}>
-                <ReactMarkdown>{loadingText}</ReactMarkdown>
+                <ReactMarkdown>{text}</ReactMarkdown>
             </div>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
